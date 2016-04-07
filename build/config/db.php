fdabf814601fe48e5cecd9b14a0fe430100bdd62
Viktor <levik04122008@yandex.ru>
2016-04-07 13:23:11 +0300

    change config db and build.xml

--- build/config/db.php
+++ build/config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=mayarossa',
-    'username' => 'root',
-    'password' => '',
+    'username' => '{{db.username}}',
+    'password' => '{{db.password}}',
     'charset' => 'utf8',
 ];
--- views/site/index.php
+++ views/site/index.php
@@ -3,6 +3,5 @@
 /* @var $this yii\web\View */
 
 $this->title = 'My Yii Application';
-
 ?>
 <h1>Добро пожаловать на <?= $domain; ?></h1>
