3199a1b79b5dd6b06aec95c550b126df6f431dc9
Mamonov Viktor <masevius@rambler.ru>
2016-04-21 23:01:01 +0300

    modify MailController

--- commands/MailController.php
+++ commands/MailController.php
@@ -10,15 +10,31 @@ class MailController extends Controller
 {
     protected function actionValidateEmail($email)
     {
-       return preg_match( "/^[a-z0-9_-]{1,20}@(([a-z0-9-]+\.)+(com|net|org|mil|".
-           "edu|gov|arpa|info|biz|inc|name|[a-z]{2})|[0-9]{1,3}\.[0-9]{1,3}\.[0-".
-           "9]{1,3}\.[0-9]{1,3})$/is", $email)? true : false;
+        return preg_match("/^[a-z0-9_-]{1,20}@(([a-z0-9-]+\.)+(com|net|org|mil|" .
+            "edu|gov|arpa|info|biz|inc|name|[a-z]{2})|[0-9]{1,3}\.[0-9]{1,3}\.[0-" .
+            "9]{1,3}\.[0-9]{1,3})$/is", $email) ? true : false;
 
     }
 
-    public function  actionSend()
+    public function actionSend()
     {
-        echo $this->actionValidateEmail();
+        $json = json_encode(
+            [
+                'UserId' => 21,
+                'template' => 'ramember',
+                'text' => 'Oplatite naonets zakaz'
+            ]);
+        $context = stream_context_create([
+            'http' => [
+                'method' => 'POST',
+                'content' => http_build_query([
+                    'data' => $json
+                ])
+            ]]);
+
+        $res = file_get_contents('http://mail/mail/test.php', null, $context);
+        print_r($res);
+
         die;
         $mail = new Mailer();
         $mail->Send();
